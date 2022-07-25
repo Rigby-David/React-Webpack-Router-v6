92482a4c413be19b43444335787da44aae70ce33
Marty Nelson <marty@martypdx.com>
2022-07-25 10:17:13 -0700

    add basic react hello world files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Layout from './components/Page/Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import List from './components/List/List.jsx';
+
+export default function App() {
+  return <div>Hello World</div>;
+}
