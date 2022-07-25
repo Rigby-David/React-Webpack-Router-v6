541933f84534941aece56a3e2996621198c76a22
Marty Nelson <marty@martypdx.com>
2022-07-25 10:22:14 -0700

    remove unneccesary imports from App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,3 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import Layout from './components/Page/Layout.jsx';
-import Home from './components/Home/Home.jsx';
-import List from './components/List/List.jsx';
-
 export default function App() {
   return <div>Hello World</div>;
 }
